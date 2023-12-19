1c20b11352b40fd3bcb192710439bbe12ac62cd9
Jose <147728854+jose-1804@users.noreply.github.com>
2023-12-19 12:06:57 -0400

    19/12/23

--- NumII.java
+++ NumII.java
@@ -8,21 +8,27 @@ public class NumII {
 
         //DeclaraFecha
         int cMonth;
-
         cMonth = Fecha.get(Calendar.MONTH);
-
 
         //DeclaraNum_Aletorios
         SecureRandom variable = new SecureRandom();
-            int contador = variable.nextInt(4000);
+            int contador = variable.nextInt(10);
           //  System.out.println(contador);
 
 
+        SecureRandom variable_II = new SecureRandom();
+            int contadorII = variable_II.nextInt(10);
+
+        SecureRandom variable_III = new SecureRandom();
+            int contadorIII = variable_III.nextInt(10);
+
+        SecureRandom variableIV = new SecureRandom();
+            int contadorIV = variableIV.nextInt(10);
 
         //Carga_Vectores  
-        System.out.println("  SERIAL TICKET");
+        System.out.println("\tSERIAL TICKET");
 
-        int [] codigoP = new int [10];
+        int [] codigoP = new int [13];
         codigoP[0] = 0;
         codigoP[1] = 0;
         codigoP[2] = 1;
@@ -32,9 +38,12 @@ public class NumII {
         codigoP[6] = 0;
         codigoP[7] = 0;
         codigoP[8] = 0;
-        codigoP[9] = contador;      
+        codigoP[9] = contador;   
+        codigoP[10] = contadorII;   
+        codigoP[11] = contadorIII;
+        codigoP[12] = contadorIV;
 
-            for(int codificador = 0; codificador < 10; codificador++) {
+            for(int codificador = 0; codificador < 13; codificador++) {
                 System.out.print(" " + codigoP[codificador]);
             }
         }
