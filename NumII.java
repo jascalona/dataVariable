2e62c9169a54ea1a6a30625654072620b097472f
Jose <147728854+jose-1804@users.noreply.github.com>
2023-12-19 11:40:17 -0400

    19/12/23

--- NumII.java
+++ NumII.java
@@ -1,17 +1,41 @@
-import java.util.Random;
+import java.util.Calendar;
+import java.security.SecureRandom;
 
 public class NumII {
+
+    static Calendar Fecha = Calendar.getInstance();
     public static void main(String [] args) {
 
-        int aleatorioII = 0;
+        //DeclaraFecha
+        int cMonth;
 
-        Random r = new Random();
-
-        aleatorioII = (int) (r.nextDouble() * 100);
-        System.out.println(aleatorioII);
-
-        
+        cMonth = Fecha.get(Calendar.MONTH);
 
 
-    }
+        //DeclaraNum_Aletorios
+        SecureRandom variable = new SecureRandom();
+            int contador = variable.nextInt(4000);
+          //  System.out.println(contador);
+
+
+
+        //Carga_Vectores  
+        System.out.println("  SERIAL TICKET");
+
+        int [] codigoP = new int [10];
+        codigoP[0] = 0;
+        codigoP[1] = 0;
+        codigoP[2] = 1;
+        codigoP[3] = cMonth;
+        codigoP[4] = 0;
+        codigoP[5] = 0;
+        codigoP[6] = 0;
+        codigoP[7] = 0;
+        codigoP[8] = 0;
+        codigoP[9] = contador;      
+
+            for(int codificador = 0; codificador < 10; codificador++) {
+                System.out.print(" " + codigoP[codificador]);
+            }
+        }
 }
