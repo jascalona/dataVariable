235e6096da10ac179f223600b37181b97f13fcad
Jose Escalona <152739008+jascalona@users.noreply.github.com>
2024-01-17 19:16:55 -0400

    Actualizar
    Prueba 17 de enero

---
+++ Pequivalente.java
@@ -0,0 +1,31 @@
+public class Pequivalente.java {
+  public static void main(String[]args) {
+
+
+   SecureRandom ale = new SecureRandom();
+     int Contador = ale.netInt(10);
+
+
+   SecureRandom aleI = new SecureRandom();
+     int ContadorI = ale.netInt(10);
+
+  
+   //Carga vectores
+   
+   int [] ComP = new int [5];
+
+   ComP[0] = 0;
+   ComP[1] = 0;
+   ComP[2] = 1;
+   ComP[3] = Contador;
+   ComP[4] = ContadorI;
+
+  
+   //Declaración Registros
+
+   int 
+   
+
+
+  }
+}
